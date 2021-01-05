572b7c7182300bc4b96ede8c420a6ea1b01a7bb3
MatteoDoyen <Matteo.Doyen@etu.univ-grenoble-alpes.fr>
2021-01-05 14:41:57 +0100

    feat : decodage des instructions pour data processing (wip)

--- arm_constants.h
+++ arm_constants.h
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T Ã  but pÃ©dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique GÃ©nÃ©rale GNU publiÃ©e par la Free Software
+Foundation (version 2 ou bien toute autre version ultÃ©rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuÃ© car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spÃ©cifique. Reportez-vous Ã  la
+Licence Publique GÃ©nÃ©rale GNU pour plus de dÃ©tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reÃ§u une copie de la Licence Publique GÃ©nÃ©rale GNU en mÃªme
+temps que ce programme ; si ce n'est pas le cas, Ã©crivez Ã  la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+Ã‰tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 BÃ¢timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'HÃ¨res
 */
 #ifndef __ARM_CONSTANTS_H__
 #define __ARM_CONSTANTS_H__
@@ -53,6 +53,24 @@ Contact: Guillaume.Huard@imag.fr
 #define LSR 1
 #define ASR 2
 #define ROR 3
+
+/* op codes */
+#define AND 0b0000
+#define EOR 0b0001
+#define SUB 0b0010
+#define RSB 0b0011
+#define ADD 0b0100
+#define ADC 0b0101
+#define SBC 0b0110
+#define RSC 0b0111
+#define TST 0b1000
+#define TEQ 0b1001
+#define CMP 0b1010
+#define CMN 0b1011
+#define ORR 0b1100
+#define MOV 0b1101
+#define BIC 0b1110
+#define MVN 0b1111
 
 /* Bit mask constants for msr */
 /* We simulate architecture v5T */
--- arm_data_processing.c
+++ arm_data_processing.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_data_processing.h"
 #include "arm_exception.h"
@@ -29,6 +29,122 @@ Contact: Guillaume.Huard@imag.fr
 
 /* Decoding functions for different classes of instructions */
 int arm_data_processing_shift(arm_core p, uint32_t ins) {
+	uint32_t result_value;
+
+	uint8_t carry_bit = get_bit(ins, 28);
+
+	uint8_t bit_i = get_bit(ins, 25);
+
+	uint8_t opcode = get_bits(ins, 24, 21);
+
+	uint8_t rn_register_number = get_bits(ins,19,16);
+	uint32_t first_operand = arm_read_register(p, rn_register_number);
+
+	uint8_t rd_register_number = get_bits(ins,15,12);
+
+	uint32_t second_operand;
+
+	if (bit_i == 0)
+	{
+		uint8_t bit_quatre = get_bit(ins, 4);
+		uint8_t shift_type = get_bits(ins,6,5);
+
+		uint8_t register_number = get_bits(ins,3,0);
+		second_operand = arm_read_register(p, register_number);
+
+		uint8_t shift_amount;
+
+		if (bit_quatre == 0)
+		{
+			shift_amount = get_bits(ins,11,7);
+
+		}
+		else
+		{
+			uint8_t register_number = get_bits(ins,11,8);
+			shift_amount = arm_read_register(p, register_number);
+		}
+
+		switch (shift_type)
+		{
+			case LSL:
+				second_operand <<= shift_amount;
+				break;
+			case LSR:
+				second_operand >>= shift_amount;
+				break;
+			case ASR:
+				second_operand = asr(second_operand, shift_amount);
+				break;
+			case ROR:
+				second_operand = ror(second_operand, shift_amount);
+				break;
+			default:
+				break;
+		}
+	}
+	else
+	{
+		second_operand = get_bits(ins,7,0);
+		uint8_t rotate_value = get_bits(ins,11,8);
+
+		second_operand = ror(second_operand, rotate_value);
+	}
+
+	//  write_register(registers r, uint8_t reg, uint32_t value)
+
+	switch (opcode)
+	{
+		case AND:
+			result_value = first_operand & second_operand;
+			break;
+		case EOR:
+			result_value = first_operand ^ second_operand;
+			break;
+		case SUB:
+			result_value = first_operand - second_operand;
+			break;
+		case RSB:
+			result_value = second_operand - first_operand;
+			break;
+		case ADD:
+			result_value = first_operand + second_operand;
+			break;
+		case ADC:
+			// oupsi
+			break;
+		case SBC:
+			// oupsi
+			break;
+		case RSC:
+			// oupsi
+			break;
+		case TST:
+			break;
+		case TEQ:
+			break;
+		case CMP:
+			break;
+		case CMN:
+			break;
+		case ORR:
+			result_value = first_operand | second_operand;
+			break;
+		case MOV:
+			result_value = second_operand;
+			break;
+		case BIC:
+			result_value = first_operand & ~second_operand;
+			break;
+		case MVN:
+			result_value = ~second_operand;
+			break;
+		default:
+			return UNDEFINED_INSTRUCTION;
+			break;
+
+	}
+
     return UNDEFINED_INSTRUCTION;
 }
 
