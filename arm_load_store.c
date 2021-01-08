713bfcccdcefba244d1bc0b9294744b442466bff
IanisOPac <ianis.olivier.pacaud@gmail.com>
2021-01-08 16:04:33 +0100

    feat: unsigned halfword load/store done

--- arm_load_store.c
+++ arm_load_store.c
@@ -100,7 +100,7 @@ int arm_load_store(arm_core p, uint32_t ins) {
     }
 
     if (bit_P == bit_W) {
-        base_address = bit_P ? base_address : base_address + offset; 
+        base_address = bit_P ? base_address : base_address + offset;
         arm_write_register(p, rn, base_address);
     }
 
@@ -164,6 +164,63 @@ int arm_load_store_multiple(arm_core p, uint32_t ins) {
             else
                 arm_write_register(p, rn, base_address - (number_bit_set * 4));
         }
+    }
+
+    return UNDEFINED_INSTRUCTION;
+}
+
+int arm_extra_load_store(arm_core p, uint32_t ins) {
+
+    uint8_t bit_S = get_bit(ins, 6);
+    uint8_t bit_H = get_bit(ins, 5);
+
+    if (bit_S || !bit_H) {
+        // pas un halfword
+    }
+
+    uint8_t bit_P = get_bit(ins, 24);
+    uint8_t bit_add = get_bit(ins, 23);
+    uint8_t bit_immediate = get_bit(ins, 22);
+    uint8_t bit_W = get_bit(ins, 21);
+    uint8_t bit_load = get_bit(ins, 20);
+
+    uint8_t rn = get_bits(ins, 19, 16);
+    uint8_t rd = get_bits(ins, 15, 12);
+    uint8_t addr_mode_H = get_bits(ins, 11, 8);
+    uint8_t addr_mode_L = get_bits(ins, 3, 0);
+
+    int32_t offset = 0;
+    uint32_t value = 0;
+    uint32_t base_address = arm_read_register(p, rn);
+
+    if (bit_immediate) {
+        offset = (addr_mode_H << 4) | addr_mode_L;
+    }
+    else {
+        offset = arm_read_register(p, addr_mode_L);
+    }
+
+    offset = bit_add ? offset : -offset;
+
+    if (bit_P) {
+        base_address += offset; 
+    }
+
+    if (bit_load) { // load
+        uint16_t temp = 0;
+        arm_read_half(p, base_address, &temp);
+        value |= temp;
+        arm_write_register(p, rd, value);
+    }
+    else { // store
+        value |= arm_read_register(p, rd);
+        arm_write_word(p, base_address, value);
+    }
+
+    if (bit_P == bit_W) {
+        if (!bit_P) 
+            base_address += offset;
+        arm_write_register(p, rn, base_address);
     }
 
     return UNDEFINED_INSTRUCTION;
