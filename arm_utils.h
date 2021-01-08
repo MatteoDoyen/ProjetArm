639dcfc1e871bf1833c02869489a1a05c5058985
jetseti <shane.moissonnier@gmail.com>
2021-01-08 18:16:24 +0100

    fix(main) : modification arm_utils.h
    Co-Authored-By: Matteo DOYEN <61158903+MatteoDoyen@users.noreply.github.com>

--- arm_utils.h
+++ arm_utils.h
@@ -8,6 +8,6 @@ void arm_update_flags(arm_core p, uint8_t n_flag, uint8_t z_flag, uint8_t c_flag
 
 int arm_check_condition(arm_core p, uint8_t condition_field);
 
-uint32_t arm_decode_shift(arm_core p, uint8_t shift_type, uint8_t shift_amount, uint8_t register_operand);
+uint32_t arm_decode_shift(arm_core p, uint8_t shift_type, uint8_t shift_amount, uint32_t second_operand);
 
 #endif
