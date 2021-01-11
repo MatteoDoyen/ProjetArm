5006330d693906fbf781faded28fbef6b901a51a
RemyDG <remy.del-grosso@outlook.com>
2021-01-11 09:32:52 +0100

    refactor(arm_branch_other) : Factorisation du code à l'aide de la fonction pour vérifier si les conditions sont correctes

--- arm_utils.h
+++ arm_utils.h
@@ -4,6 +4,9 @@
 #include <stdint.h>
 #include "arm_core.h"
 
+#define PASSED 1
+#define UNPASSED 0
+
 void arm_update_flags(arm_core p, uint8_t n_flag, uint8_t z_flag, uint8_t c_flag, uint8_t v_flag);
 
 int arm_check_condition(arm_core p, uint8_t condition_field);
